f741efa246f862601251f5e07ca1b57f1f87f7a3
yaosuncoder <47038849+yaosuncoder@users.noreply.github.com>
2021-08-09 00:35:56 +0800

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+public class Java {
+    static boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') stack.push(')');
+            else if(s.charAt(i) == '[') stack.push(']');
+            else if(s.charAt(i) == '{') stack.push('}');
+            else {
+                if(stack.isEmpty() || stack.pop()!=s.charAt(i))
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        System.out.println(isValid("(){}["));
+    }
+}
